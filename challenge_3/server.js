4bfd4e95fc84875eaf8eba2d3244761527274092
Collin-St <collin.street.dr@gmail.com>
2018-12-12 12:41:51 -0800

    Serving up index.html, rendering react component through server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.render('server');
+
+app.get('/', (req, res) => {
+  // res.send()
+  console.log('GET received');
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
