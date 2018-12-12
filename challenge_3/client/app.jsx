dc29fb574fcdba4fd49f5ced3a90a3abcff0e620
Collin-St <collin.street.dr@gmail.com>
2018-12-12 15:44:07 -0800

    Rendering react component with a title and checkout button on the homepage

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hello, world!</h1>
+        <button>Checkout</button>
+      </div>
+    )
+  }
+};
+
+
+
 
 ReactDOM.render(
-  <h1>hello, world!!!</h1>,
-  // <App />,
+  <App />,
   document.getElementById('app')
 );
 
